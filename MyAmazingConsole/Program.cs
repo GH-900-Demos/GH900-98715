6820ab5aaf878abfb6ab0c74ff1a357a0835bf49
Massimo Bonanni <massimo.bonanni@tiscali.it>
2026-03-25 12:47:27 +0100

    Implement DES key and IV generation
    Added DES encryption key and IV generation.

--- MyAmazingConsole/Program.cs
+++ MyAmazingConsole/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+using System.Security.Cryptography;
+
+var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
+des.GenerateKey();
+des.GenerateIV();
+
+Console.WriteLine("Hello, World!");
